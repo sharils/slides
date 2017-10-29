31a40b07a1f965226afc3ff21b8362c01a674ee9
sharils <sharils@users.noreply.github.com>
2017-10-29 17:38:28 +0800

    Remove dead code

--- jscodeshift-refactor-js-with-js/demo-1/__test__/demo-1-transform.test.js
+++ jscodeshift-refactor-js-with-js/demo-1/__test__/demo-1-transform.test.js
@@ -1,6 +1,4 @@
 jest.autoMockOff();
 const defineTest = require('jscodeshift/dist/testUtils').defineTest;
-const defineInlineTest = require('jscodeshift/dist/testUtils').defineInlineTest;
-const transform = require('../demo-1-transform');
 
 defineTest(__dirname, 'demo-1-transform');
--- jscodeshift-refactor-js-with-js/demo-7/__test__/demo-7-transform.test.js
+++ jscodeshift-refactor-js-with-js/demo-7/__test__/demo-7-transform.test.js
@@ -1,6 +1,4 @@
 jest.autoMockOff();
 const defineTest = require('jscodeshift/dist/testUtils').defineTest;
-const defineInlineTest = require('jscodeshift/dist/testUtils').defineInlineTest;
-const transform = require('../demo-7-transform');
 
 defineTest(__dirname, 'demo-7-transform');
